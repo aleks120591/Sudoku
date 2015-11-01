d289ec58394b68041df1e0d7533fae8e7583d27a
warlock <aleks120591@gmail.com>
2015-11-01 20:53:28 +0400

    Initial commit

--- app/src/main/java/com/example/sudoku/Sudoku.java
+++ app/src/main/java/com/example/sudoku/Sudoku.java
@@ -10,7 +10,7 @@ public class Sudoku extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sudoku);
+        setContentView(R.layout.main);
     }
 
     @Override
