127cc6ab165688b2962ef67b2542643e1652da45
warlock <aleks120591@gmail.com>
2015-11-01 21:25:30 +0400

    Initial commit

--- app/src/main/java/com/example/sudoku/About.java
+++ app/src/main/java/com/example/sudoku/About.java
@@ -1,9 +1,16 @@
 package com.example.sudoku;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Александр on 01.11.2015.
  */
 public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.about);
+
+    }
 }
